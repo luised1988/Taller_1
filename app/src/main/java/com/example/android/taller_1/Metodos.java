3a73759677a17c343b073ca9b640ee6f62da6657
luised1988 <ldiazd1988@gmail.com>
2017-09-30 13:30:01 -0500

    Taller Testeado

---
+++ app/src/main/java/com/example/android/taller_1/Metodos.java
@@ -0,0 +1,143 @@
+package com.example.android.taller_1;
+
+/**
+ * Created by android on 30/09/2017.
+ */
+
+public class Metodos {
+
+    public static double Hombre_Zapatilla_Nike(double cant,double opcion1, double opcion2,double opcion3){
+
+        double resultado=0;
+
+        if (opcion1 == 0) {
+            if (opcion2 == 0) {
+                if (opcion3 == 0) {
+                    resultado = 120000 * cant;
+
+                }
+            }
+        }
+
+
+        if (opcion1 == 0) {
+            if (opcion2 == 0) {
+                if (opcion3 == 1) {
+                    resultado = 140000 * cant;
+
+                }
+            }
+
+        }
+
+        if (opcion1 == 0) {
+            if (opcion2 == 0) {
+                if (opcion3 == 2) {
+                    resultado = 130000 * cant;
+
+                }
+            }
+
+        }
+
+        if (opcion1 == 0) {
+            if (opcion2 == 1) {
+                if (opcion3 == 0) {
+                    resultado = 50000 * cant;
+
+                }
+            }
+
+        }
+
+        if (opcion1 == 0) {
+            if (opcion2 == 1) {
+                if (opcion3 == 1) {
+                    resultado = 80000 * cant;
+
+                }
+            }
+
+        }
+
+        if (opcion1 == 0) {
+            if (opcion2 == 1) {
+                if (opcion3 == 2) {
+                    resultado = 100000 * cant;
+
+                }
+            }
+
+        }
+
+
+        if (opcion1 == 1) {
+            if (opcion2 == 0) {
+                if (opcion3 == 0) {
+                    resultado = 100000 * cant;
+
+                }
+            }
+
+        }
+
+        if (opcion1 == 1) {
+            if (opcion2 == 0) {
+                if (opcion3 == 1) {
+                    resultado = 130000 * cant;
+
+                }
+            }
+
+        }
+
+        if (opcion1 == 1) {
+            if (opcion2 == 0) {
+                if (opcion3 == 2) {
+                    resultado = 110000 * cant;
+
+                }
+            }
+
+        }
+
+        if (opcion1 == 1) {
+            if (opcion2 == 1) {
+                if (opcion3 == 0) {
+                    resultado = 60000 * cant;
+
+                }
+            }
+
+        }
+
+        if (opcion1 == 1) {
+            if (opcion2 == 1) {
+                if (opcion3 == 1) {
+                    resultado = 70000 * cant;
+
+                }
+            }
+
+        }
+
+        if (opcion1 == 1) {
+            if (opcion2 == 1) {
+                if (opcion3 == 2) {
+                    resultado = 120000 * cant;
+
+                }
+            }
+
+        }
+
+
+
+
+
+        return resultado;
+    }
+
+
+
+}
--- app/src/test/java/com/example/android/taller_1/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/taller_1/ExampleUnitTest.java
@@ -10,8 +10,126 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void Valor_correspondiente_Correcto1(){
+        assertEquals(240000,Metodos.Hombre_Zapatilla_Nike(2,0,0,0),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto2(){
+        assertEquals(280000,Metodos.Hombre_Zapatilla_Nike(2,0,0,1),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto3(){
+        assertEquals(130000,Metodos.Hombre_Zapatilla_Nike(1,0,0,2),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto4(){
+        assertEquals(50000,Metodos.Hombre_Zapatilla_Nike(1,0,1,0),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto5(){
+        assertEquals(80000,Metodos.Hombre_Zapatilla_Nike(1,0,1,1),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto6(){
+        assertEquals(100000,Metodos.Hombre_Zapatilla_Nike(1,0,1,2),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto7(){
+        assertEquals(100000,Metodos.Hombre_Zapatilla_Nike(1,1,0,0),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto8(){
+        assertEquals(130000,Metodos.Hombre_Zapatilla_Nike(1,1,0,1),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto9(){
+        assertEquals(110000,Metodos.Hombre_Zapatilla_Nike(1,1,0,2),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto10(){
+        assertEquals(60000,Metodos.Hombre_Zapatilla_Nike(1,1,1,0),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto11(){
+        assertEquals(70000,Metodos.Hombre_Zapatilla_Nike(1,1,1,1),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_Correcto12(){
+        assertEquals(120000,Metodos.Hombre_Zapatilla_Nike(1,1,1,2),0);
+    }
+
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto1(){
+        assertNotEquals(20000,Metodos.Hombre_Zapatilla_Nike(2,0,0,0),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto2(){
+        assertNotEquals(20000,Metodos.Hombre_Zapatilla_Nike(2,0,0,1),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto3(){
+        assertNotEquals(10000,Metodos.Hombre_Zapatilla_Nike(1,0,0,2),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto4(){
+        assertNotEquals(5000,Metodos.Hombre_Zapatilla_Nike(1,0,1,0),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto5(){
+        assertNotEquals(8000,Metodos.Hombre_Zapatilla_Nike(1,0,1,1),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto6(){
+        assertNotEquals(10000,Metodos.Hombre_Zapatilla_Nike(1,0,1,2),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto7(){
+        assertNotEquals(10000,Metodos.Hombre_Zapatilla_Nike(1,1,0,0),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto8(){
+        assertNotEquals(13000,Metodos.Hombre_Zapatilla_Nike(1,1,0,1),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto9(){
+        assertNotEquals(11000,Metodos.Hombre_Zapatilla_Nike(1,1,0,2),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto10(){
+        assertNotEquals(6000,Metodos.Hombre_Zapatilla_Nike(1,1,1,0),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto11(){
+        assertNotEquals(7000,Metodos.Hombre_Zapatilla_Nike(1,1,1,1),0);
+    }
+
+    @Test
+    public void Valor_correspondiente_NoCorrecto12(){
+        assertNotEquals(12000,Metodos.Hombre_Zapatilla_Nike(1,1,1,2),0);
     }
 }
